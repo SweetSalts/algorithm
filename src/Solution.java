32303d3c75e5df8382a18bc7ef365e87ba9903ec
yansiwei <ysiwei97@163.com>
2017-07-11 22:24:41 +0800

    顺时针打印矩阵中的元素

--- src/Solution.java
+++ src/Solution.java
@@ -282,4 +282,55 @@ public class Solution {
             return false;
         return root1.val==root2.val && IsSubtree(root1.left,root2.left) && IsSubtree(root1.right,root2.right);
     }
+    //顺时针打印数组输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
+    // 例如，如果输入如下矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    // 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+    public ArrayList<Integer> printMatrix(int [][] matrix) {
+        int column = matrix[0].length;
+        int row = matrix.length;
+        int start = 0;
+        ArrayList<Integer> b = new ArrayList();
+        //只有一行
+        if (row == 1) {
+            for (int i = 0; i < column; i++) {
+                b.add(matrix[0][i]);
+            }
+            return b;
+        }
+        //只有一列
+        if (column == 1) {
+            for (int i = 0; i < row; i++) {
+                b.add(matrix[i][0]);
+            }
+            return b;
+        }
+        while (column > 2 * start && row > 2 * start) {
+            int x = column - 1 - start;
+            int y = row - 1 - start;
+            //从左到右打印
+            for (int i = start; i <= x; i++) {
+                b.add(matrix[start][i]);
+            }
+            //从上到下打印
+            if (start < y) {
+                for (int i = start + 1; i <= y; i++) {
+                    b.add(matrix[i][x]);
+                }
+            }
+            //从右往左打印
+            if (start < x && start < y) {
+                for (int i = x - 1; i >= start; i--) {
+                    b.add(matrix[y][i]);
+                }
+            }
+            //从下往上打印
+            if (start < x && start < y - 1) {
+                for (int i = y - 1; i >= start + 1; i--) {
+                    b.add(matrix[i][start]);
+                }
+            }
+            start++;
+        }
+        return b;
+    }
 }
