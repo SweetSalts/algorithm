561f39e1ec24d52e9e42677ed8a1cba7d8a4772c
yansiwei <ysiwei97@163.com>
2017-07-13 22:03:44 +0800

    寻找二叉树中和为target的路径+复杂链表的复制

--- src/Solution.java
+++ src/Solution.java
@@ -377,4 +377,59 @@ public class Solution {
             right=VerifySquenceOfBST(Arrays.copyOfRange(sequence,indexleft,sequence.length-1));
         return left&&right;
     }
+    //输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+    // 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+    ArrayList<ArrayList<Integer>> array = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> a = new ArrayList<Integer>();
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        if(root == null)
+            return array;
+        a.add(root.val);
+        target -= root.val;
+        if(target==0 && root.left==null && root.right==null)
+            array.add(new ArrayList<Integer>(a));
+        FindPath(root.left,target);
+        FindPath(root.right,target);
+        a.remove(a.size()-1);
+        return array;
+    }
+    //复杂链表的复制
+    //输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，
+    // 另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
+    public class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+    public RandomListNode Clone(RandomListNode pHead){
+        if(pHead == null)
+            return null;
+        RandomListNode cur = pHead;
+        while(cur != null){
+            RandomListNode newnode = new RandomListNode(cur.label);
+            newnode.next = cur.next;
+            cur.next = newnode;
+            cur = newnode.next;
+        }
+        cur = pHead;
+        while(cur!=null){
+            if(cur.random!=null){
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+        cur = pHead;
+        RandomListNode cloneHead = pHead.next;
+        RandomListNode temp = null;
+        while(cur.next!=null){
+            temp = cur.next;
+            cur.next = temp.next;
+            cur = temp;
+        }
+        return cloneHead;
+    }
 }
